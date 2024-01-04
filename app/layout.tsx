b03dc97aef69b38a332fd6e72ee905a0a7a164ef
shylabo <shuya.sohot@gmail.com>
2024-01-04 12:49:53 -0800

    fix: font issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from 'next';
-import { Inter as FontSans } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { AuthProvider } from '@/providers/AuthProvider';
 
-export const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Foodely',
@@ -18,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={cn('grid grid-cols-1 min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+      <body className={cn('grid grid-cols-1 min-h-screen bg-background antialiased', inter.className)}>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
