5361ffbb5247fac6eeb09bd039b4ce34baa18b99
shylabo <shuya.sohot@gmail.com>
2024-01-04 15:00:57 -0800

    update: footer link

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,10 +9,10 @@ const Footer = () => {
         <Image src={'/image/foodely-logo.png'} alt={'Foodely logo'} width={120} height={40} />
       </Link>
       <div className="flex items-center justify-center gap-x-10">
-        <p>© {new Date().getFullYear()} All Rights Reserved</p>
-        <Link href={''} className="text-primary underline cursor-not-allowed">
-          Privacy Policy & Terms of Use
-        </Link>
+        <p>© {new Date().getFullYear()} All Rights Reserved. Shuya Sonoda</p>
+        <a href="https://www.shuya-life.com" target="_blank" className="text-primary underline">
+          Personal website
+        </a>
       </div>
     </footer>
   );
